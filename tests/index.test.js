b544ca784e70c025380c52d0a2be6744a88f566d
slayeh17 <debakroy7@gmail.com>
2024-05-03 02:59:55 +0530

    add step 2

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,28 @@
 // tests/index.test.js
 
-test('Basic Jest Test', () => {
+test("Basic Jest Test", () => {
     expect(1).toBe(1);
-  });
+});
+
+// src/csvReader.js
+
+const fs = require("fs");
+const csv = require("csv-parser");
+
+function readCSV(filePath) {
+    const results = [];
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on("data", (data) => results.push(data))
+            .on("end", () => {
+                resolve(results);
+            })
+            .on("error", (error) => {
+                reject(error);
+            });
+    });
+}
+
+module.exports = readCSV;
